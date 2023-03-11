351c4e6f82ff06a03ab60b0fef22c40272fa6996
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2023-03-11 15:15:20 +0900

    feat : first Settings

---
+++ src/main/java/com/design/pattern/gofdessgnpattern_in_spring/GoFDeSsgnPatternInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.design.pattern.gofdessgnpattern_in_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFDeSsgnPatternInSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoFDeSsgnPatternInSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/design/pattern/gofdessgnpattern_in_spring/GoFDeSsgnPatternInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.pattern.gofdessgnpattern_in_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoFDeSsgnPatternInSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
